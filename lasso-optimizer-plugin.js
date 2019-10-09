97d66acc9c4ef3cb36e42dddc38e6913dff054e5
dasathyakuma <dasathyakuma@ebay.com>
2019-10-09 16:26:41 -0600

    Setup the transform

---
+++ lasso-optimizer-plugin.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const parseAndGenerateCode = require('./lib');
+
+module.exports = (lasso, pluginConfig) => {
+    lasso.addTransform({
+        // Only apply to JS code
+        contentType: 'js',
+
+        // Give your module a friendly name
+        // (helpful for debugging in case something goes wrong in your code)
+        name: module.id,
+
+        // If stream is set to false then a String will be provided.
+        // Otherwise, a readable stream will be provided
+        stream: false,
+
+        // Do the magic:
+        transform: (code, lassoContext) => parseAndGenerateCode(code, pluginConfig)
+    });
+};
